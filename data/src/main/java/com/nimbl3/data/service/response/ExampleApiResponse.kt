8b214031853f467b87350461573ea34542f3c754
Metas <oakkub.1995@gmail.com>
2018-06-14 15:59:31 +0700

    Initial commit :)

---
+++ data/src/main/java/com/nimbl3/data/service/response/ExampleApiResponse.kt
@@ -0,0 +1,14 @@
+package com.nimbl3.data.service.response
+
+data class ExampleResponse(val data: ExampleDataResponse)
+
+data class ExampleDataResponse(
+    val children: List<ExampleChildrenResponse>
+)
+
+data class ExampleChildrenResponse(val data: ExampleChildrenDataResponse)
+
+data class ExampleChildrenDataResponse(
+    val author: String,
+    val title: String
+)
